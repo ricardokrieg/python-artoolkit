05f699e34f0cf19cdc908070213a73e942535519
Ricardo Krieg <ricardo.krieg@gmail.com>
2012-09-14 00:44:23 -0300

    xxx

--- artoolkitmodule.cpp
+++ artoolkitmodule.cpp
@@ -11,13 +11,13 @@ class ARToolKit {
     public:
         ARToolKit(void) {
             char vconf[] = "v4l2src device=/dev/video0 ! ffmpegcolorspace ! capsfilter caps=video/x-raw-rgb,bpp=24 ! identity name=artoolkit ! fakesink";
-            if(arVideoOpen( vconf ) < 0) exit(0);
+            if (arVideoOpen( vconf ) < 0) exit(0);
 
-            if(arVideoInqSize(&this->xsize, &this->ysize) < 0) exit(0);
+            if (arVideoInqSize(&this->xsize, &this->ysize) < 0) exit(0);
 
             char cparam_name[] = "Data/camera_para.dat";
             ARParam wparam, cparam;
-            if(arParamLoad(cparam_name, 1, &wparam) < 0) {
+            if (arParamLoad(cparam_name, 1, &wparam) < 0) {
                 printf("Camera parameter load error !!\n");
                 exit(0);
             }
@@ -28,13 +28,52 @@ class ARToolKit {
             arParamDisp(&cparam);
 
             char patt_name[] = "Data/patt.hiro";
-            if((this->patt_id=arLoadPatt(patt_name)) < 0) {
+            if ((this->patt_id=arLoadPatt(patt_name)) < 0) {
                 printf("pattern load error !!\n");
                 exit(0);
             }
+
+            arVideoCapStart();
+
+            this->count = 0;
+            this->patt_center[0] = 0.0;
+            this->patt_center[1] = 0.0;
+        }
+
+        void update() {
+            ARUint8 *dataPtr;
+            ARMarkerInfo *marker_info;
+            int marker_num;
+            int j, k;
+
+            if ((dataPtr = (ARUint8 *)arVideoGetImage()) == NULL) {
+                arUtilSleep(2);
+                return;
+            }
+            if (count == 0) arUtilTimerReset();
+            count++;
+
+            if (arDetectMarker(dataPtr, this->thresh, &marker_info, &marker_num) < 0) {
+                this->close();
+                exit(0);
+            }
+
+            arVideoCapNext();
+
+            k = -1;
+            for (j = 0; j < marker_num; j++) {
+                if (this->patt_id == marker_info[j].id) {
+                    if (k == -1) k = j;
+                    else if (marker_info[k].cf < marker_info[j].cf) k = j;
+                }
+            }
+            if (k == -1) return;
+
+            arGetTransMat(&marker_info[k], this->patt_center, this->patt_width, this->patt_trans);
         }
 
         void close(void) {
+            arVideoCapStop();
             arVideoClose();
         }
 
@@ -44,17 +83,34 @@ class ARToolKit {
             return ret;
         }
 
+        BP::tuple get_matrix(void) {
+            BP::tuple ret_0 = BP::make_tuple(this->patt_trans[0][0], this->patt_trans[0][1], this->patt_trans[0][2]);
+            BP::tuple ret_1 = BP::make_tuple(this->patt_trans[1][0], this->patt_trans[1][1], this->patt_trans[1][2]);
+            BP::tuple ret_2 = BP::make_tuple(this->patt_trans[2][0], this->patt_trans[2][1], this->patt_trans[2][2]);
+
+            BP::tuple ret = BP::make_tuple(ret_0, ret_1, ret_2);
+
+            return ret;
+        }
+
     private:
         int xsize, ysize;
         int patt_id;
+        int count;
+        static const int thresh = 100;
+        static const double patt_width = 80.0;
+        double patt_center[2];
+        double patt_trans[3][4];
 };
 
 BOOST_PYTHON_MODULE(artoolkit) {
     using namespace boost::python;
 
     class_<ARToolKit>("ARToolKit", init<>())
+        .def("update", &ARToolKit::update)
         .def("close", &ARToolKit::close)
 
-        .add_property("size", &ARToolKit::get_size);
+        .add_property("size", &ARToolKit::get_size)
+        .add_property("matrix", &ARToolKit::get_matrix);
     ;
 }
--- build/lib.linux-x86_64-2.7/artoolkit.py
+++ build/lib.linux-x86_64-2.7/artoolkit.py
@@ -1,4 +1,11 @@
 from artoolkit import *
 
 artoolkit = ARToolKit()
+
+while True:
+	artoolkit.update()
+
+	print artoolkit.matrix
+# while
+
 artoolkit.close()
