593539ff90b5a8fd863fac873dc95cde695a7ca7
Ricardo Franco <ricardo.krieg@gmail.com>
2012-09-17 16:59:30 -0300

    ...

--- examples/artoolkit.py
+++ examples/artoolkit.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import pygame
 from pygame.locals import *
 import cv
@@ -5,57 +7,105 @@ import inspect
 import numpy
 from math import *
 
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
 from artoolkit import *
+
+def draw_surface(surface):
+	glMatrixMode(GL_PROJECTION)
+	glPushMatrix()
+	glLoadIdentity()
+	glOrtho(0, 1, 0, 1, 0, 1)
+
+	glMatrixMode(GL_MODELVIEW)
+	glPushMatrix()
+	glLoadIdentity()
+
+	glDepthMask(False)
+	glEnable(GL_TEXTURE_2D)
+
+	textureData = pygame.image.tostring(surface, "RGBA", 1)
+	width = surface.get_width()
+	height = surface.get_height()
+	texture = glGenTextures(1)
+	glBindTexture(GL_TEXTURE_2D, texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+	glBegin(GL_QUADS)
+	glTexCoord2d(0, 0)
+	glVertex2f(0, 0)
+	glTexCoord2d(1, 0)
+	glVertex2f(1, 0)
+	glTexCoord2d(1, 1)
+	glVertex2f(1, 1)
+	glTexCoord2d(0, 1)
+	glVertex2f(0, 1)
+	glEnd()
+
+	glDepthMask(True)
+	glDisable(GL_TEXTURE_2D)
+
+	glPopMatrix()
+	glMatrixMode(GL_PROJECTION)
+	glPopMatrix()
+	glMatrixMode(GL_MODELVIEW)
+# draw_surface
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480), HWSURFACE|OPENGL|DOUBLEBUF)
+pygame.display.set_caption('ARToolKit')
+glutInit()
 
 artoolkit = ARToolKit()
 size = artoolkit.size
 
-pygame.init()
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('ARToolKit')
-
-font = pygame.font.SysFont(None, 36)
-
 running = True
 while running:
 	for event in pygame.event.get():
-		if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-			running = False
+		if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE: running = False
 	# for
 
-	screen.fill((0, 0, 0))
-
 	artoolkit.update()
-	matrix = artoolkit.matrix
-	gl_matrix = artoolkit.gl_matrix
 
-	print "%3.1f %3.1f %3.1f %3.1f \n\
-%3.1f %3.1f %3.1f %3.1f\n\
-%3.1f %3.1f %3.1f %3.1f\n\
-%3.1f %3.1f %3.1f %3.1f\n" % (
-		gl_matrix[0][0], gl_matrix[0][1], gl_matrix[0][2], gl_matrix[0][3], 
-		gl_matrix[1][0], gl_matrix[1][1], gl_matrix[1][2], gl_matrix[1][3], 
-		gl_matrix[2][0], gl_matrix[2][1], gl_matrix[2][2], gl_matrix[2][3], 
-		gl_matrix[3][0], gl_matrix[3][1], gl_matrix[3][2], gl_matrix[3][3], 
-	)
-
+	glClear(GL_COLOR_BUFFER_BIT)
+	glColor3f(1, 1, 1)
+	
 	frame = numpy.asarray(artoolkit.frame, dtype=numpy.uint8).reshape(size[1], size[0], 3)
 	image = cv.fromarray(frame)
 	pyimage = pygame.image.frombuffer(image.tostring(), cv.GetSize(image), 'RGB')
+	draw_surface(pyimage)
 
-	screen.blit(pyimage, (0, 0))
+	if artoolkit.visible:
+		artoolkit.draw3d()
 
-	pos = artoolkit.pos
-	x = int(pos[0])
-	y = int(pos[1])
+		ambient = [0.0, 0.0, 1.0, 1.0]
+		flash = [0.0, 0.0, 1.0, 1.0]
+		flash_shiny = [50.0]
+		light_position = [100.0,-200.0,200.0,0.0]
+		ambi = [0.1, 0.1, 0.1, 0.1]
+		light_zero_color = [0.9, 0.9, 0.9, 0.1]
+	    
+		glEnable(GL_DEPTH_TEST)
+		glEnable(GL_LIGHTING)
+		glEnable(GL_LIGHT0)
+		glLightfv(GL_LIGHT0, GL_POSITION, light_position)
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambi)
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_zero_color)
+		glMaterialfv(GL_FRONT, GL_SPECULAR, flash)
+		glMaterialfv(GL_FRONT, GL_SHININESS, flash_shiny)
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient)
 
-	angle = asin(gl_matrix[0][0])
+		glMatrixMode(GL_MODELVIEW)
+		glTranslatef( 0.0, 0.0, 25.0 )
+		glutSolidCube(50.0)
 
-	try:
-		pygame.draw.line(screen, (255, 0, 0), (x, y), (x+50*gl_matrix[0][1], y-50*gl_matrix[0][0]), 3)
-		pygame.draw.line(screen, (0, 0, 255), (x, y), (x+50*gl_matrix[0][0], y+50*gl_matrix[0][1]), 3)
-		pygame.draw.circle(screen, (0, 255, 0), (x, y), 5)
-	except: pass
+		glDisable(GL_DEPTH_TEST)
+		glDisable(GL_LIGHTING)
+	# if
 
 	pygame.display.flip()
 # while
--- src/artoolkitmodule.cpp
+++ src/artoolkitmodule.cpp
@@ -13,16 +13,6 @@ namespace BP = boost::python;
 class ARToolKit {
     public:
         ARToolKit(void) {
-            this->count = 0;
-            this->patt_center[0] = 0.0;
-            this->patt_center[1] = 0.0;
-            static GLuint glid[4];
-
-            char argv0[] = "artoolkit";
-            char *argv[] = {argv0};
-            int argc = 1;
-            glutInit(&argc, argv);
-
             char vconf[] = "v4l2src device=/dev/video0 ! video/x-raw-yuv,width=640,height=480 ! ffmpegcolorspace ! capsfilter caps=video/x-raw-rgb,bpp=24 ! identity name=artoolkit ! fakesink";
             // char vconf[] = "v4l2src device=/dev/video0 ! ffmpegcolorspace ! capsfilter caps=video/x-raw-rgb,bpp=24 ! identity name=artoolkit ! fakesink";
             if (arVideoOpen( vconf ) < 0) exit(0);
@@ -47,70 +37,26 @@ class ARToolKit {
                 exit(0);
             }
 
-            static int gImXsize = cparam.xsize;
-            static int gImYsize = cparam.ysize;
-
-            static ARParam gCparam;
+            static ARParam  gCparam;
             gCparam = cparam;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; ++i) {
                 gCparam.mat[1][i] = (gCparam.ysize-1)*(gCparam.mat[2][i]) - gCparam.mat[1][i];
             }
-            argConvGLcpara(&gCparam, AR_GL_CLIP_NEAR, AR_GL_CLIP_FAR, this->gl_cpara);
-
-            static int    tex1Xsize1 = 1;
-            static int    tex1Xsize2 = 1;
-            static int    tex1Ysize  = 1;
-            static int    tex2Xsize  = 1;
-            static int    tex2Ysize  = 1;
-
-            glGenTextures(4, glid);
-            glBindTexture( GL_TEXTURE_2D, glid[0] );
-            glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-            glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );
-            glBindTexture( GL_TEXTURE_2D, glid[1] );
-            glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-            glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );
-            glBindTexture( GL_TEXTURE_2D, glid[2] );
-            glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-            glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );
-
-            if( gImXsize > 512 ) {
-                tex1Xsize1 = 512;
-                tex1Xsize2 = 1;
-                while( tex1Xsize2 < gImXsize - tex1Xsize1 ) tex1Xsize2 *= 2;
-            }
-            else {
-                tex1Xsize1 = 1;
-                while( tex1Xsize1 < gImXsize ) tex1Xsize1 *= 2;
-            }
-            tex1Ysize  = 1;
-            while( tex1Ysize < gImYsize ) tex1Ysize *= 2;
-
-            tex2Xsize = 1;
-            while( tex2Xsize < gImXsize/2 ) tex2Xsize *= 2;
-            tex2Ysize = 1;
-            while( tex2Ysize < gImYsize/2 ) tex2Ysize *= 2;
+            argConvGLcpara( &gCparam, AR_GL_CLIP_NEAR, AR_GL_CLIP_FAR, this->gl_cpara );
 
             arVideoCapStart();
 
-            // glMatrixMode(GL_PROJECTION);
-            // glLoadIdentity();
-            // gluPerspective(100, 1, 0.5, 500);
-            // glMatrixMode(GL_MODELVIEW);
-            // glLoadIdentity();
-            // gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
+            this->count = 0;
+            this->patt_center[0] = 0.0;
+            this->patt_center[1] = 0.0;
+            this->visible = false;
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluPerspective(100, 1, 0.5, 500);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
         }
 
         void update() {
@@ -122,11 +68,8 @@ class ARToolKit {
                 arUtilSleep(2);
                 return;
             }
-            if (this->count == 0) arUtilTimerReset();
-            this->count++;
-
-            argDrawMode2D();
-            argDispImage(dataPtr, 0, 0);
+            if (count == 0) arUtilTimerReset();
+            count++;
 
             if (arDetectMarker(this->dataPtr, this->thresh, &marker_info, &marker_num) < 0) {
                 this->close();
@@ -136,54 +79,60 @@ class ARToolKit {
             arVideoCapNext();
 
             k = -1;
+            this->visible = true;
             for (j = 0; j < marker_num; j++) {
                 if (this->patt_id == marker_info[j].id) {
                     if (k == -1) k = j;
                     else if (marker_info[k].cf < marker_info[j].cf) k = j;
                 }
             }
-            if (k == -1) return;
+            if (k == -1) {
+                this->visible = false;
+                return;
+            }
 
             arGetTransMat(&marker_info[k], this->patt_center, this->patt_width, this->patt_trans);
-            // argConvGlpara(this->patt_trans, this->gl_para);
-
-            argDrawMode3D();
-            argDraw3dCamera(0, 0);
-            glClearDepth(1.0);
-            glClear(GL_DEPTH_BUFFER_BIT);
-            // glEnable(GL_DEPTH_TEST);
-            // glDepthFunc(GL_LEQUAL);
-
             argConvGlpara(this->patt_trans, this->gl_para);
+        }
+
+        void draw3d(void) {
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadMatrixd(this->gl_cpara);
+
+            glClearDepth( 1.0 );
+            glClear(GL_DEPTH_BUFFER_BIT);
+            glDepthFunc(GL_LEQUAL);
+
             glMatrixMode(GL_MODELVIEW);
             glLoadMatrixd(this->gl_para);
-
-            // glEnable(GL_LIGHTING);
-            // glEnable(GL_LIGHT0);
-            // glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-            // glLightfv(GL_LIGHT0, GL_AMBIENT, ambi);
-            // glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor);
-            // glMaterialfv(GL_FRONT, GL_SPECULAR, mat_flash);
-            // glMaterialfv(GL_FRONT, GL_SHININESS, mat_flash_shiny);  
-            // glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-            // glMatrixMode(GL_MODELVIEW);
-            // glTranslatef( 0.0, 0.0, 25.0 );
-            // glutSolidCube(50.0);
-            // glDisable( GL_LIGHTING );
-
-            // glDisable( GL_DEPTH_TEST );
         }
 
         void close(void) {
             arVideoCapStop();
             arVideoClose();
-            argCleanup();
         }
 
         BP::tuple get_size(void) {
             BP::tuple ret = BP::make_tuple(this->xsize, this->ysize);
 
             return ret;
+        }
+
+        BP::list get_frame(void) {
+            BP::list ret;
+
+            for (unsigned int i=0; i<(this->xsize*this->ysize*3); i++) {
+                ret.append(this->dataPtr[i]);
+            }
+
+            return ret;
+        }
+
+        bool is_visible(void) {
+            return this->visible;
         }
 
     private:
@@ -196,6 +145,7 @@ class ARToolKit {
         double patt_trans[3][4];
         double gl_para[16], gl_cpara[16];
         ARUint8 *dataPtr;
+        bool visible;
 };
 
 BOOST_PYTHON_MODULE(artoolkit) {
@@ -203,8 +153,11 @@ BOOST_PYTHON_MODULE(artoolkit) {
 
     class_<ARToolKit>("ARToolKit", init<>())
         .def("update", &ARToolKit::update)
+        .def("draw3d", &ARToolKit::draw3d)
         .def("close", &ARToolKit::close)
 
         .add_property("size", &ARToolKit::get_size)
+        .add_property("frame", &ARToolKit::get_frame)
+        .add_property("visible", &ARToolKit::is_visible);
     ;
 }
